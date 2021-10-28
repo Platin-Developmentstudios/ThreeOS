baba6ee309cfa19e94ccb6592dad92852a6a3c6d
Dominik H <64731738+TheDomCraft@users.noreply.github.com>
2021-10-28 22:16:25 +0200

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace ThreeOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Cyan);
+            Console.WriteLine("| ThreeOS Beta Build 2810 | Created By Platin Developmentstudios |", ConsoleColor.Cyan);
+            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Cyan);
+        }
+
+        protected override void Run()
+        {
+            Console.Write("root@ThreeOS > ");
+            var input = Console.ReadLine();
+
+            if(input == "help")
+            {
+                Console.WriteLine("- sysinfo | Show infos about the ThreeOS build");
+            } else if(input == "sysinfo")
+            {
+                Console.WriteLine("User    | root@ThreeOS");
+                Console.WriteLine("OS      | ThreeOS Beta");
+                Console.WriteLine("Version | Build 2810");
+            }
+        }
+    }
+}
