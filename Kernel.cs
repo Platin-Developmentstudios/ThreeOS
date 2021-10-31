f59af9e338f6eb36cd3ad52994a9b8189a19ab5a
Dominik H <64731738+TheDomCraft@users.noreply.github.com>
2021-10-31 10:21:48 +0100

    Build 3110
    This Build Contains Mayor Changes
    
    Changelog:
       - Added Colors to the Screen

--- Kernel.cs
+++ Kernel.cs
@@ -1,61 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+using System.Collections.Generic;
 using Sys = Cosmos.System;
 using Cosmos.System.Graphics;
-using System.Drawing;
 
 namespace ThreeOS
 {
     public class Kernel : Sys.Kernel
     {
-
-        //Canvas canvas;
-
         protected override void BeforeRun()
         {
-
-            //canvas = FullScreenCanvas.GetFullScreenCanvas();
-            //canvas.Clear(Color.Wheat);
-
-            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Cyan);
-            Console.WriteLine("| ThreeOS Beta Build 3010 | Created By Platin Developmentstudios |", ConsoleColor.Cyan);
-            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Cyan);
-            Console.WriteLine("| To see all commands, type in help                              |", ConsoleColor.Cyan);
-            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Cyan);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Magenta);
+            Console.WriteLine("| ThreeOS Beta Build 3110 | Created By Platin Developmentstudios |", ConsoleColor.Magenta);
+            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Magenta);
+            Console.WriteLine("| To see all commands, type in help                              |", ConsoleColor.Magenta);
+            Console.WriteLine("------------------------------------------------------------------", ConsoleColor.Magenta);
         }
 
         protected override void Run()
         {
-            Console.WriteLine("root@ThreeOS > ");
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("root@ThreeOS > ", ConsoleColor.White);
             var input = Console.ReadLine();
 
             if (input == "help")
             {
 
-                Console.WriteLine("---------------------------------------------------");
-                Console.WriteLine("| sysinfo    | Show infos about the ThreeOS build |");
-                Console.WriteLine("| clear      | Clear the Console                  |");
-                Console.WriteLine("| shutdown   | Shut down the PC                   |");
-                Console.WriteLine("| restart    | Restart the PC                     |");
-                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("---------------------------------------------------", ConsoleColor.White);
+                Console.WriteLine("| sysinfo    | Show infos about the ThreeOS build |", ConsoleColor.White);
+                Console.WriteLine("| clear      | Clear the Console                  |", ConsoleColor.White);
+                Console.WriteLine("| shutdown   | Shut down the PC                   |", ConsoleColor.White);
+                Console.WriteLine("| restart    | Restart the PC                     |", ConsoleColor.White);
+                Console.WriteLine("---------------------------------------------------", ConsoleColor.White);
 
             } else if (input == "sysinfo")
             {
 
-                Console.WriteLine("----------------------------");
-                Console.WriteLine("| User      | root@ThreeOS |");
-                Console.WriteLine("| OS        | ThreeOS Beta |");
-                Console.WriteLine("| Version   | Build 3010   |");
-                Console.WriteLine("----------------------------");
+                Console.WriteLine("----------------------------", ConsoleColor.White);
+                Console.WriteLine("| User      | root@ThreeOS |", ConsoleColor.White);
+                Console.WriteLine("| OS        | ThreeOS Beta |", ConsoleColor.White);
+                Console.WriteLine("| Version   | Build 3110   |", ConsoleColor.White);
+                Console.WriteLine("----------------------------", ConsoleColor.White);
                 Console.WriteLine(" ");
-                Console.WriteLine("Hardware:");
-                Console.WriteLine($"Total RAM: {Cosmos.Core.CPU.GetAmountOfRAM()} MB ");
-                Console.WriteLine($"CPU: {Cosmos.Core.CPU.GetCPUVendorName()}");
+                Console.WriteLine("Hardware:", ConsoleColor.White);
+                Console.WriteLine($"Total RAM: {Cosmos.Core.CPU.GetAmountOfRAM()} MB ", ConsoleColor.White);
+                Console.WriteLine($"CPU: {Cosmos.Core.CPU.GetCPUVendorName()}", ConsoleColor.White);
 
             } else if (input == "clear")
             {
 
+                Console.BackgroundColor = ConsoleColor.DarkGray;
                 Console.Clear();
 
             } else if (input == "shutdown")
