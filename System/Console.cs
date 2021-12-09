b9d0c0b6accc0c84fa5ed886763ed95e58f7e53f
Dominik H <64731738+TheDomCraft@users.noreply.github.com>
2021-12-09 18:17:37 +0100

    Update Console.cs
    Fixed Write() error

--- System/Console.cs
+++ System/Console.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace ThreeOS.System.Console
 {
@@ -6,32 +7,32 @@ namespace ThreeOS.System.Console
   {
     public static void OK(string text)
     {
-      Write("\n[ OK ] ", ConsoleColor.Green);
-      Write(text, ConsoleColor.White);
+      Console.Write("\n[ OK ] ", ConsoleColor.Green);
+      Console.Write(text, ConsoleColor.White);
     }
     
     public static void Wait(string text)
     {
-      Write("\n[ ... ] ", ConsoleColor.Cyan);
-      Write(text, ConsoleColor.White);
+      Console.Write("\n[ ... ] ", ConsoleColor.Cyan);
+      Console.Write(text, ConsoleColor.White);
     }
     
     public static void Error(string text)
     {
-      Write("\n[ ERROR ] ", ConsoleColor.Red);
-      Write(text, ConsoleColor.White);
+      Console.Write("\n[ ERROR ] ", ConsoleColor.Red);
+      Console.Write(text, ConsoleColor.White);
     }
     
     public static void Warning(string text)
     {
-      Write("\n[ WARNING ] ", ConsoleColor.Yellow);
-      Write(text, ConsoleColor.White);
+      Console.Write("\n[ WARNING ] ", ConsoleColor.Yellow);
+      Console.Write(text, ConsoleColor.White);
     }
     
     public static void Info(string text)
     {
-      Write("\n[ INFO ] ", ConsoleColor.Cyan);
-      Wrtite(text, ConsoleColor.White);
+      Console Write("\n[ INFO ] ", ConsoleColor.Cyan);
+      Console.Wrtite(text, ConsoleColor.White);
     }
   }
 }
