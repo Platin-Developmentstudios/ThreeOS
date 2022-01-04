c6d24fe89af3efdb9aa06247a372ecbbfc85a669
Dominik H <64731738+TheDomCraft@users.noreply.github.com>
2022-01-04 02:05:29 +0100

    Updated ThreeScript

--- System/ThreeScriptEngine.cs
+++ System/ThreeScriptEngine.cs
@@ -5,35 +5,46 @@ using ThreeOS.System.ConsoleTools;
 using ThreeOS.System.ThreeScript.CMD;
 
 namespace ThreeOS.System.ThreeScript {
+    
     internal class TScriptEngine {
+        
         public static void Execute(string filename) {
+            
             try {
+                
                 if((filename.EndsWith(".ths")) || (filename.EndsWith(".threescript"))) {
+                    
                     string[] lines = file.ReadAllLines(filename);
                     foreach(string line in lines) {
+                        
                         if(!(line.StartsWith("|"))) {
-                            switch(line.StartsWith()) {
-                                case "echo":
-                                    var echoContent = line.Remove(0, 5);
-                                    TScriptCmdManager.EchoCMD(echoContent);
-                                    break;
+                            
+                            if(line.StartsWith("echo")) {
+                                
+                                string echoContent = line.Remove(0, 5);
+                                TScriptCmdManager.EchoCMD(echoContent);
+                                
+                            } else if(line.StartsWith("run")) {
+                                
+                                var runFile = line.Remove(0, 4);
+                                TScriptCmdManager.RunCMD(runFile);
                                     
-                                case "run":
-                                    var runFile = line.Remove(0, 4);
-                                    TScriptCmdManager.RunCMD(runFile)
-                                    
-                                default:
-                                    break;
+                            } else {
+                                
+                                Console.WriteLine("> INT <");
+                                
                             }
                         }
                     }
-                } else 
-                {
+                } else {
+                    
                     ConsoleInfo.Error("This is not a valid ThreeScript!");
+                    
                 }
-            } catch(Exception e)
-            {
+            } catch(Exception e) {
+                
                 ConsoleInfo.Error(e.ToString());
+                
             }
         }
     }
