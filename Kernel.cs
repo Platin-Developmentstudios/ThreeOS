f3a3479d01fe67624b866f733e9dec7241a7ae87
Dominik H <64731738+TheDomCraft@users.noreply.github.com>
2022-01-04 02:07:08 +0100

    Updated to Build 04012022
    Changelog:
        - I really changed everything

--- Kernel.cs
+++ Kernel.cs
@@ -16,7 +16,7 @@ namespace ThreeOS
         public static string CurrentDirectory = @"0:\system\mnt\";
         public static string ComputerName = "ThreeOS";
         public static string CurrentUser = "Admin";
-        public static string BuildVer = "01012022";
+        public static string BuildVer = "04012022";
 
         protected override void BeforeRun() {
 
@@ -68,128 +68,123 @@ namespace ThreeOS
 
             var input = Console.ReadLine();
             
-            switch(input) {
+            if(input.StartsWith("help") {
                 
-                case "help":
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("-----------------------------------------------------");
-                    Console.WriteLine("| sysinfo    | Show infos about the ThreeOS build   |");
-                    Console.WriteLine("| clear      | Clear the Console                    |");
-                    Console.WriteLine("| shutdown   | Shut down the PC                     |");
-                    Console.WriteLine("| restart    | Restart the PC                       |");
-                    Console.WriteLine("| cd         | Change the Current dir               |");
-                    Console.WriteLine("| ls         | List all files in a dir              |");
-                    Console.WriteLine("| mkfile     | Create a new file in the current dir |");
-                    Console.WriteLine("| mkdir      | Create a new dir in the current dir  |");
-                    Console.WriteLine("| run        | Run a ThreeScript File               |");
-                    Console.WriteLine("| echo       | Print text to the Console            |")
-                    Console.WriteLine("-----------------------------------------------------");
-                    break;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("| sysinfo    | Show infos about the ThreeOS build   |");
+                Console.WriteLine("| clear      | Clear the Console                    |");
+                Console.WriteLine("| shutdown   | Shut down the PC                     |");
+                Console.WriteLine("| restart    | Restart the PC                       |");
+                Console.WriteLine("| cd         | Change the Current dir               |");
+                Console.WriteLine("| ls         | List all files in a dir              |");
+                Console.WriteLine("| mkfile     | Create a new file in the current dir |");
+                Console.WriteLine("| mkdir      | Create a new dir in the current dir  |");
+                Console.WriteLine("| run        | Run a ThreeScript File               |");
+                Console.WriteLine("| echo       | Print text to the Console            |")
+                Console.WriteLine("-----------------------------------------------------");
                     
-                case "sysinfo":
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("--------------------------------");
-                    Console.WriteLine("| User      | Admin            |");
-                    Console.WriteLine("| OS        | ThreeOS Beta     |");
-                    Console.WriteLine("| Version   | Build 01012022   |");
-                    Console.WriteLine("--------------------------------");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("Hardware:");
-                    Console.WriteLine($"Total RAM: {Cosmos.Core.CPU.GetAmountOfRAM()} MB ");
-                    Console.WriteLine($"CPU: {Cosmos.Core.CPU.GetCPUVendorName()}");
-                    break;
+            } else if(input.StartsWith("sysinfo")) {
+                
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("| User      | Admin            |");
+                Console.WriteLine("| OS        | ThreeOS Beta     |");
+                Console.WriteLine("| Version   | Build 04012022   |");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine(" ");
+                Console.WriteLine("Hardware:");
+                Console.WriteLine($"Total RAM: {Cosmos.Core.CPU.GetAmountOfRAM()} MB ");
+                Console.WriteLine($"CPU: {Cosmos.Core.CPU.GetCPUVendorName()}");
+                 
+            } else if(input.StartsWith("clear")) {
+            
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.White;
+            
+            } else if(input.StartsWith("shutdown")) {
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Shutdowning PC...");
+                System.Threading.Thread.Sleep(3000);
+                Sys.Power.Shutdown();
                     
-                case "clear":
-                    Console.Clear();
-                    Console.BackgroundColor = ConsoleColor.DarkGray;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
+            } else if(input.StartsWith("restart")) {
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Restarting PC...");
+                System.Threading.Thread.Sleep(3000);
+                Sys.Power.Reboot();
+            
+            } else if(input.StartsWith("cd")) {
+                
+                string dirPath = input.Remove(0, 3);
+                if (!Directory.Exists(dirPath)) {
+                    ConsoleInfo.Error("This Directory doesnt exist!");
+                } else {
+                    CurrentDirectory = dirPath;
+                }
                     
-                case "shutdown":
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Shutdowning PC...");
-                    System.Threading.Thread.Sleep(3000);
-                    Sys.Power.Shutdown();
-                    break;
-                    
-                case "restart":
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Restarting PC...");
-                    System.Threading.Thread.Sleep(3000);
-                    Sys.Power.Reboot();
-                    break;
-                    
-                case "cd":
-                    string dirPath = input.Remove(0, 3);
-                    if (!Directory.Exists(dirPath)) {
-                        ConsoleInfo.Error("This Directory doesnt exist!");
-                    } else {
-                        CurrentDirectory = dirPath;
+            } else if(input.StartsWith("ls")) {
+                
+                string directory_list = Directory.GetFiles(CurrentDirectory);
+                foreach(var file in directory_list) {
+                    Console.WriteLine(file);
+                }
+                
+            } else if(input.StartsWith("mkfile")) {
+                
+                string filename = input.Remove(0, 7);
+                if(filename == "") {
+                    ConsoleInfo.Error("Please enter a File name");
+                } else {
+                    try {
+                        var file_stream = File.Create(CurrentDirectory + filename);
+                    } catch(Exception e) {
+                        ConsoleInfo.Error(e.ToString());
                     }
-                    break;
+                    ConsoleInfo.OK("File successfully created.");
+                }
                     
-                case "ls"
-                    var directory_list = Directory.GetFiles(CurrentDirectory);
-                    foreach(var file in directory_list) {
-                        Console.WriteLine(file);
-                    }
-                    break;
-                    
-                case "mkfile":
-                    string filename = input.Remove(0, 7);
-                    if(filename == "") {
-                        ConsoleInfo.Error("Please enter a File name");
-                    } else
-                    {
+            } else if(input.StartsWith("mkdir")) {
+                
+                if (input == "") {
+                    Console.WriteLine("Please enter an dir name!");
+                } else {
+                    if(!Directory.Exists(dirname)) {
                         try {
-                            var file_stream = File.Create(CurrentDirectory + filename);
-                        } catch(Exception e)
-                        {
+                            var directory_stream = Directory.Create(CurrentDirectory + dirname);
+                        }
+                        catch(Exception e) {
                             ConsoleInfo.Error(e.ToString());
                         }
-                        ConsoleInfo.OK("File successfully created.");
-                    }
-                    break;
-                    
-                case "mkdir":
-                    if (input == "") {
-                        Console.WriteLine("Please enter an dir name!");
-                    } else
-                    {
-                        if(!Directory.Exists(dirname)) {
-                            try
-                            {
-                                var directory_stream = Directory.Create(CurrentDirectory + dirname);
-                            }
-                            catch(Exception e) {
-                                ConsoleInfo.Error(e.ToString());
-                            }
-                            ConsoleInfo.OK("Directory successfully created.");
-                        } else
-                        {
-                            ConsoleInfo.Error("This Directory already exists!");
-                        }
-                    }
-                    break;
-                    
-                case "run":
-                    string TScriptPath = input.Remove(0, 4);
-                    if(TScriptPath == "") {
-                        ConsoleInfo.Error("Not a valid ThreeScript File!");
+                        ConsoleInfo.OK("Directory successfully created.");
                     } else {
-                        TScriptEngine.Execute(TScriptPath);
+                        ConsoleInfo.Error("This Directory already exists!");
                     }
-                    break;
+                }
                     
-                case "echo":
-                    string echoString = input.Remove(0, 5);
-                    Console.WriteLine(echoString);
-                    break;
+            } else if(input.StartsWith("run")) {
                 
-                default:
-                    ConsoleInfo.Error("Command not Found");
-                    break;
+                string TScriptPath = input.Remove(0, 4);
+                if(TScriptPath == "") {
+                    ConsoleInfo.Error("Not a valid ThreeScript File!");
+                } else {
+                    TScriptEngine.Execute(TScriptPath);
+                }
+                    
+            } else if(input.StartsWith("echo")) {
+                
+                string echoString = input.Remove(0, 5);
+                Console.WriteLine(echoString);
+                    
+            } else {
+            
+                ConsoleInfo.Error("Command not Found");
+                
             }
         }
     }
 }
+
