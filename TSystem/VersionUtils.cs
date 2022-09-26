c5c12ed286fa9cca37f5bf2d4a1655e7137712f4
GreaterSlash3 <114431187+GreaterSlash3@users.noreply.github.com>
2022-09-26 23:10:37 +0200

    Cleaning all this Shit up
    I removed everything from the code that was either useless or didn't make sense

--- TSystem/VersionUtils.cs
+++ TSystem/VersionUtils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,8 @@ namespace ThreeOS.TSystem.VersionUtils
 {
     internal class VersionInfo
     {
-        public static string BuildVersion = "20042022";
-        public static string PublicVersion = "Beta 1.0.0.0";
-        public static string InternalReleaseVersion = "20.2022.04";
+        public static string BuildVersion = "26092022";
+        public static string PublicVersion = "Beta 1.7.0.0";
+        public static string InternalReleaseVersion = "26.2022.09";
     }
 }
